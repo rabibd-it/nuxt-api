795e8ada1888d711552b8e801d04e63a47fc0dcb
Rabiul Islam <rabibd.it@gmail.com>
2023-09-03 04:51:58 +0600

    statuctare

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div>
+        wowo
+    </div>
 </template>
---
+++ frontend/components/AppLogo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtLink to="/" class="flex items-center">
+      <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo">
+      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+        LaraNuxt
+      </span>
+    </NuxtLink>
+  </div>
+</template>
